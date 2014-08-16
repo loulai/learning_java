8c4ddadddcc0e41bb9002841e2390c6df600ddf6
Louise <louiselai88@gmail.com>
2014-08-16 19:06:41 +1000

    iterating using a foreach

--- Looping/src/Main.java
+++ Looping/src/Main.java
@@ -5,8 +5,12 @@ public class Main {
 		{"Jan", "Feb", "March", "April"};
 
 	public static void main(String[] args) {
-		for (int i = 0; i< months.length ; i++) {
-			System.out.println(months[i]);
+//		for (int i = 0; i< months.length ; i++) {
+//			System.out.println(months[i]);
+//		}
+		
+		for (String elem : months) {
+			System.out.println(elem);
 		}
 
 	}
