37792905ce7adff171a6e4f4b45e47ed5b974be3
Louise <louiselai88@gmail.com>
2014-08-20 20:58:08 +0800

    added new project for method overloading

---
+++ MethodOverloading/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int mainResult = Main.addValues(3, 2);
+		System.out.println(mainResult);
+	}
+	
+	private static int addValues(int s1, int s2){
+		int result = s1 + s2;
+		return result;
+	}
+
+}
