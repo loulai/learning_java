38db9f644c91297dbb3cddad0a3d60c9923ff169
Louise <louiselai88@gmail.com>
2014-08-21 17:47:10 -0400

    practising nested for loops and doing a number challenge

---
+++ NestedLoops/src/Lesson.java
@@ -0,0 +1,13 @@
+//import java.util.Scanner
+
+public class Lesson {
+
+	public static void main(String[] args) {
+	
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Hello " + i);
+		}
+
+	}
+
+}
