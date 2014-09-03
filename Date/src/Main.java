c1f58ef01a332c64d9ac5d85451144b602eb1b88
Louise <louiselai88@gmail.com>
2014-09-03 17:40:59 -0400

    can work with Date class from java.util

---
+++ Date/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Date rightNow = new Date();
+		System.out.println(rightNow);
+	}
+
+}
