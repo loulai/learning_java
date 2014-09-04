71c68e9895ec6e17526fa709e5c9909de65ed2e5
Louise <louiselai88@gmail.com>
2014-09-04 15:50:45 -0400

    creating two dimensional arrays

---
+++ TwoDimensionalArrays/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String[][] countries = new String[3][2];
+		
+		countries[0][0] = "Malaysia";
+		countries[0][1] = "KL";
+		
+		countries[1][0] = "Australia";
+		countries[1][1] = "Canberra";
+		
+		countries[2][0] = "China";
+		countries[2][1] = "Beijing";
+		
+	}
+
+}
